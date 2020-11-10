14325f81f673fd8ff0d480e4e373633c4d9f9a75
josimar-jr <shikamarujunior@gmail.com>
2020-11-10 00:09:06 -0300

    corrige formato de saída do resultado para ser do tipo numérico

--- src/multiplication/MultiplicationController.kt
+++ src/multiplication/MultiplicationController.kt
@@ -1,5 +1,5 @@
 package org.josimarjr.msmulti.multiplication
 
-fun multiplicationController(factor1: Double, factor2: Double): Map<String, String> {
-    return mapOf("result" to multiplicationService(factor1, factor2).toString())
+fun multiplicationController(factor1: Double, factor2: Double): Map<String, Double> {
+    return mapOf("result" to multiplicationService(factor1, factor2))
 }
